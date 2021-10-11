ca058ab14f5bec7ae5d7582acaa800ccdbf58808
DasCodes <dascodes@pm.me>
2021-10-11 12:15:12 +0530

    fix baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'EarthMC',
   tagline: 'A Geopolitical Sandbox',
   url: 'https://earthmc.net',
-  baseUrl: '/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
